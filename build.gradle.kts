febb7ecc491096afac0bc4d40d956fbc33840fa4
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-20 19:22:17 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://developer.huawei.com/repo/") }
+    }
+    dependencies {
+        classpath("com.huawei.agconnect:agcp:1.9.1.301")
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.googleServices) apply false
-    alias(libs.plugins.huaweiAgconnect) apply false
     alias(libs.plugins.firebaseCrashlytics) apply false
 }
