18a9a6436a9c58a56dd47cc375b47129c3635691
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-20 18:42:06 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,28 @@
 pluginManagement {
-  repositories {
-    mavenCentral()
-    google()
-    maven { url = uri("https://developer.huawei.com/repo/") }
-  }
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://developer.huawei.com/repo/") }
+        gradlePluginPortal()
+    }
 }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    mavenCentral()
-    google()
-    maven { url = uri("https://developer.huawei.com/repo/") }
-  }
-  versionCatalogs {
-    create("libs") {
-      from(files("libs.versions.toml"))
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://developer.huawei.com/repo/") }
     }
-  }
 }
 
 rootProject.name = "Ietha"
-include(":TMessagesProj")
-include(":TMessagesProj_App")
-include(":TMessagesProj_AppHuawei")
-include(":TMessagesProj_AppHockeyApp")
-include(":TMessagesProj_AppStandalone")
-include(":TMessagesProj_AppTests")
+
+include(
+    ":TMessagesProj",
+    ":TMessagesProj_App",
+    ":TMessagesProj_AppHuawei",
+    ":TMessagesProj_AppHockeyApp",
+    ":TMessagesProj_AppStandalone",
+    ":TMessagesProj_AppTests"
+)
