a639fa3fa8545ce0edd8bc08eeddb5132fc7e425
𝙸𝚊𝚝𝚑𝚎𝚒𝚢𝚊 <iatheiya@gmail.com>
2026-01-20 19:17:22 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-  alias(libs.plugins.androidApplication)
-  alias(libs.plugins.googleServices)
-  alias(libs.plugins.firebaseCrashlytics)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kotlinJvm) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.huaweiAgconnect) apply false
+    alias(libs.plugins.firebaseCrashlytics) apply false
 }
